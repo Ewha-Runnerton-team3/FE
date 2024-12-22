c87955dee23a50b8b65555838e066b4340aa1153
inaemon <github32377@gmail.com>
2024-12-23 05:24:47 +0900

    🎨 design: 메인 페이지 디자인

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,31 @@
 import Header from "../../components/layout/Header"
 import MenuBar from "../../components/layout/MenuBar"
+import dishImage from '../../assets/dish.webp';
+import refrigeratorImage from '../../assets/refrigerator.webp';
 
 const Main = () => {
     return (
-        <div>
+        <>
           <Header/>
           <MenuBar/>
-        </div>
+          <div className="bg-lemon-10 h-screen flex justify-center space-x-[80px] ">
+            <div className="py-20 space-y-10">
+              <p className="text-[35px] font-regular">
+                "당신을 위한 레시피"
+              </p>
+              <p className="text-[50px] font-bold">
+                쿳핏
+              </p>
+              <img className="w-[400px]" src={dishImage} alt="완성된 음식" />
+            </div>
+            <div className="py-[100px] space-y-10">
+              <img className="w-[400px] rounded-2xl" src={refrigeratorImage} alt="냉장고 음식" />
+              <div className="text-goldbrown-10 font-bold text-[16px] bg-yellow-80 rounded-lg text-center py-2 cursor-pointer hover:text-yellow-0">
+                레시피 추천받으러 가기
+              </div>
+            </div>
+          </div>
+        </>
     );
 }
 
