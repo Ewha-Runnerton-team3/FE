e2341a4eae9e0fdf3fd8a0968e22642fac71c053
inaemon <github32377@gmail.com>
2024-12-23 08:16:57 +0900

    🚀 feat: 즐겨찾기 페이지 토글 로직 구현

--- src/pages/Favorite/Favorite.tsx
+++ src/pages/Favorite/Favorite.tsx
@@ -1,11 +1,50 @@
+import React, { useState } from "react";
 import Header from "../../components/layout/Header"
 import MenuBar from "../../components/layout/MenuBar"
 
 const Favorite = () => {
+    // 레시피/재료 토글 상태
+    const [toggle, setToggle] = useState<number>(0);
+    
     return (
         <div>
           <Header/>
           <MenuBar/>
+
+          {/* 조건부 렌더링 */}
+          {toggle === 0? (
+            <>
+              {/** 토글 버튼 */}
+              <div className="flex bg-lemon-10 font-semibold text-lg px-[40px] py-2">
+                <p className="text-goldbrown-90 mr-10 cursor-pointer"
+                   onClick={() => setToggle(0)}>
+                  내가 저장한 레시피
+                </p>
+                <p className="text-grayscale-50 cursor-pointer"
+                   onClick={() => setToggle(1)}>
+                  내가 저장한 재료
+                </p>
+              </div>
+              
+              {/** 렌더링 */}
+            </>
+          ) : (
+            <>
+              {/** 토글 버튼 */}
+              <div className="flex bg-lemon-10 font-semibold text-lg px-[40px] py-2">
+                <p className="text-grayscale-50 mr-10 cursor-pointer"
+                  onClick={() => setToggle(0)}>
+                  내가 저장한 레시피
+                </p>
+                <p className="text-goldbrown-90 cursor-pointer"
+                  onClick={() => setToggle(1)}>
+                  내가 저장한 재료
+                </p>
+              </div>
+              
+              {/** 렌더링 */}
+            </>
+          )}
         </div>
     );
 }
