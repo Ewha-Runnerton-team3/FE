8f68b59b0c6224a6a7ab1289470abb38e3b32726
inaemon <github32377@gmail.com>
2024-12-22 18:42:48 +0900

    🎨 design: 기본 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 
+import Main from "./pages/Main/Main"
+
 function App() {
 
   return (
     <>
+      <Main/>
     </>
   )
 }
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <div>
+          <a
+            className="cursor-pointer"
+            href="/"
+          >
+            CookFit
+          </a>
+        </div>
+    );
+}
+
+export default Header;
