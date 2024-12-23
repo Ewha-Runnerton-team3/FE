a923aa71981225200fcacea448c9e1004787f8d4
inaemon <github32377@gmail.com>
2024-12-23 11:16:09 +0900

    🎨 design: 레시피 추천 결과 로직 구현

--- src/pages/Recommend/Recommend.tsx
+++ src/pages/Recommend/Recommend.tsx
@@ -4,15 +4,23 @@ import MenuBar from "../../components/layout/MenuBar"
 
 import Input from "./components/Input"
 import Result from "./components/Result"
+import { Recipe } from "../../api/RecipeApiType.ts";
 
 const Recommend = () => {
     // 제출 여부를 위한 상태
     const [submitted, setSubmitted] = useState(false);
+    // 레시피 리스트
+    const [recipeList, setRecipeList] = useState<Recipe[]>([]);
     
     // 전송 버튼 클릭 시 실행되는 함수
     const handleSubmit = () => {
       // 입력 상태를 'submitted'로 변경
       setSubmitted(true);
+
+      // API 호출
+
+      // Recipe 리스트 업데이트
+      //setRecipeList()
     };
     
 
@@ -27,7 +35,7 @@ const Recommend = () => {
               handleSubmit={handleSubmit}
             />
           ) : (
-            <Result/>
+            <Result totalItems={recipeList}/>
           )}
         </div>
     );
--- src/pages/Recommend/components/Result.tsx
+++ src/pages/Recommend/components/Result.tsx
@@ -1,10 +1,45 @@
 import React, { useState } from "react";
+import { Recipe } from "../../../api/RecipeApiType.ts";
 
-const RecommendResult = () => {
+interface Props {
+    totalItems: Recipe[]; // 부모로부터 받은 totalItems
+}
+
+const RecommendResult: React.FC<Props> = ({ totalItems }) => {
     return (
         <div>
           <div className="flex bg-lemon-10 text-goldbrown-90 font-semibold text-lg px-[40px] py-2">
             추천 결과
+          </div>
+
+          {/** 추천 레시피 결과 */}
+          <div className="p-10 space-y-10">
+            {totalItems.map((recipe, index) => (
+              <div key={index} className="flex p-10 w-full h-[150px] rounded-lg bg-yellow-30">
+                <p className="flex font-semibold text-lg p-5 space-x-10">
+                  {/* 인덱스 출력 */}
+                  {index}
+
+                  {/* 레시피 이름 출력 */}
+                  {recipe.name}
+
+                  <div>
+                    <div>
+                      <p>조리 예상 시간</p>
+                      {recipe.totalTime}
+                    </div>
+                    <div>
+                      <p>조리 난이도</p>
+                      {recipe.difficulty}
+                    </div>
+                    <div>
+                      <p>조리 스코어</p>
+                      {recipe.difficultyScore}
+                    </div>
+                  </div>
+                </p>
+              </div>
+            ))}
           </div>
           
         </div>
